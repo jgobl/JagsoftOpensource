ae01eab620f03d8b9a673d5d8bba1177582c3bae
jgobl <jgobl@live.co.uk>
2014-06-11 23:20:28 +0100

    JagsoftOpensource

---
+++ HtmlHelpers/ActionLinkHelpers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.Routing;
+
+namespace HtmlHelpers
+{
+    public static class ActionLinkHelpers
+    {
+        public static MvcHtmlString BootstrapHighlightedActionLink(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName, RouteValueDictionary routeValues, IDictionary<string, Object> htmlAttributes, string activeClass)
+        {
+            if (actionName.Equals(htmlHelper.ViewContext.RouteData.Values["action"].ToString(), StringComparison.OrdinalIgnoreCase)
+                && controllerName.Equals(htmlHelper.ViewContext.RouteData.Values["controller"].ToString(), StringComparison.OrdinalIgnoreCase))
+            {
+                if (htmlAttributes == null)
+                {
+                    htmlAttributes = new Dictionary<string, object>();
+                    htmlAttributes["class"] = activeClass;
+                }
+                else
+                {
+                    string existingClasses = htmlAttributes.ContainsKey("class") ? htmlAttributes["class"].ToString() : string.Empty;
+                    htmlAttributes["class"] = string.Format("{0} {1}", existingClasses, activeClass);
+                }
+
+            }
+
+            return htmlHelper.ActionLink(linkText, actionName, controllerName, routeValues, htmlAttributes);
+        }
+    }
+}
