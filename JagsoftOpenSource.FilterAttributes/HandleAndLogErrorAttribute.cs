a3a6835571ba41509dac3dff542c96f8f2e23e12
jgobl <jgobl@live.co.uk>
2014-06-17 11:40:54 +0100

    Added FilterAttributes project

---
+++ JagsoftOpenSource.FilterAttributes/HandleAndLogErrorAttribute.cs
@@ -0,0 +1,21 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace JagsoftOpenSource.FilterAttributes
+{
+    public class HandleAndLogErrorAttribute : HandleErrorAttribute
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(HandleAndLogErrorAttribute));
+
+        public override void OnException(ExceptionContext filterContext)
+        {
+            log.Error(string.Format("Url {0} - {1}", filterContext.HttpContext.Request.Url, filterContext.Exception.ToString()));
+            base.OnException(filterContext);
+        }
+    }
+}
