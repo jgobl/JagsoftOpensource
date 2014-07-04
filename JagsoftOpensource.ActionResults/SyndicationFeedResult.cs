69eb3b623d6c2852c33bb90290dbcaf7cddb6419
jgobl <jgobl@live.co.uk>
2014-07-04 13:51:25 +0100

    added Action Results project with SyndicationFeedResult

---
+++ JagsoftOpensource.ActionResults/SyndicationFeedResult.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace JagsoftOpensource.ActionResults
+{
+    public class SyndicationFeedResult : ActionResult
+    {
+        #region private Members
+
+        private SyndicationFeedResultsParameters syndicationFeedResultsParameters;
+
+        #endregion
+
+        #region Public Properties
+
+        public SyndicationFeedResultsParameters SyndicationFeedResultsParameters
+        {
+            get
+            {
+                return syndicationFeedResultsParameters;
+            }
+        }
+
+
+        #endregion
+
+
+        #region Public Constructors
+
+        public SyndicationFeedResult(SyndicationFeedResultsParameters syndicationFeedResultsParameters)
+        {
+            this.syndicationFeedResultsParameters = syndicationFeedResultsParameters;
+        }
+
+        #endregion
+
+        #region Public Overrides
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var response = context.RequestContext.HttpContext.Response;
+
+            response.ContentType = syndicationFeedResultsParameters.ContentType;
+
+            using (var xmlWriter = new XmlTextWriter(response.Output))
+            {
+                xmlWriter.Formatting = Formatting.Indented;
+                syndicationFeedResultsParameters.FeedFormatter.WriteTo(xmlWriter);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ JagsoftOpensource.ActionResults/SyndicationFeedResultsParameters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JagsoftOpensource.ActionResults
+{
+    public class SyndicationFeedResultsParameters
+    {
+        public SyndicationFeedFormatter FeedFormatter { get; set; }
+        public string ContentType { get; set; }
+    }
+}
