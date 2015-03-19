d3ef598710b14f0f1bf007c02c4209d93e7b3310
jgobl <jgobl@live.c.uuk>
2015-03-19 14:24:14 +0000

    updated packages

--- JagsoftOpenSource.FilterAttributes/Properties/AssemblyInfo.cs
+++ JagsoftOpenSource.FilterAttributes/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("JagsoftOpenSource.FilterAttributes")]
-[assembly: AssemblyDescription("Filter Attributes Assembley")]
+[assembly: AssemblyDescription("Custom Filter Attributes")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jagsoft")]
 [assembly: AssemblyProduct("JagsoftOpenSource.FilterAttributes")]
--- JagsoftOpenSource.HtmlHelpers/Properties/AssemblyInfo.cs
+++ JagsoftOpenSource.HtmlHelpers/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JagsoftOpenSource")]
-[assembly: AssemblyDescription("JagsoftOpenSource")]
+[assembly: AssemblyTitle("JagsoftOpenSource.HtmlHelpers")]
+[assembly: AssemblyDescription("Custom Html Helpers")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jagsoft")]
-[assembly: AssemblyProduct("JagsoftOpenSource")]
+[assembly: AssemblyProduct("JagsoftOpenSource.HtmlHelpers")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
